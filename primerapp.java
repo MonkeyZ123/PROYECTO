453e237c0055c1332fc82f1e0e5c548c2150d12d
MonkeyZ123 <151459331+MonkeyZ123@users.noreply.github.com>
2023-11-19 21:55:15 -0500

    Add files via upload
    Proyecto Java

---
+++ primerapp.java
@@ -0,0 +1,47 @@
+/app
+    /src
+        /main
+            /res
+                /drawable (nine-patch)
+                /values
+                    strings.xml (para español)
+                /values-en
+                    strings.xml (para inglés)
+                /values-fr
+                    strings.xml (para francés)
+                /values-de
+                    strings.xml (para alemán)
+                /values-it
+                    strings.xml (para italiano)
+                /values-pt
+                    strings.xml (para portugués)
+            /java
+                com.tuapp
+                    MainActivity.java
+<resources>
+    <string name="app_name">Mi Primera Aplicación</string>
+    <string name="welcome_message">¡Hola, buenos días!</string>
+    <!-- Otras cadenas en español -->
+</resources>
+
+<resources>
+    <string name="app_name">My First App</string>
+    <string name="welcome_message">Hello, good morning!</string>
+    <!-- Otras cadenas en inglés -->
+</resources>
+
+<resources>
+    <string name="app_name">La Mia Prima App</string>
+    <string name="welcome_message">Ciao, buongiorno!</string>
+    <!-- Otras cadenas en italiano -->
+</resources>
+
+<resources>
+    <string name="app_name">Minha Primeira App</string>
+    <string name="welcome_message">Olá, bom dia!</string>
+    <!-- Otras cadenas en portugués -->
+</resources>
+
+
+
+
